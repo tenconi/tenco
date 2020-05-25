0719a2aff975b2f70aa37430bac4cec02b678618
tenconi <47565305+tenconi@users.noreply.github.com>
2020-05-25 12:56:50 -0300

    arreglando LOCATION

--- php/send.php
+++ php/send.php
@@ -1,11 +1,11 @@
 <?php
 $destino="tenco216@gmail.com";
+$from="tenco@tenco.com.ar";
 $nombre=$_POST["nombre"];
 $correo=$_POST["correo"];
 $mensaje=$_POST["mensaje"];
 $contenido="Nombre: " . $nombre ."\nCorreo: " . $correo ."\nMensaje: " .$mensaje;
 mail($destino, "Consulta WEB", $contenido);
-header("Location:#thanks")
-
+header("Location:http://tenco.com.ar/tenco/#thanks");
 /*echo "Muchas gracias por el texto";*/
 ?>
