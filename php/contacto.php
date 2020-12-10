e1c8e21698a1bd7be3736787615cdc19d7bd0370
tenconi <47565305+tenconi@users.noreply.github.com>
2020-12-09 21:09:55 -0300

    funciona en Localhost

--- php/contacto.php
+++ php/contacto.php
@@ -15,12 +15,12 @@ $email = $_POST["email"];
 $mensaje = $_POST["mensaje"];
 
 // Datos de la cuenta de correo utilizada para enviar vía SMTP
-$smtpHost = "usuario.ferozo.com";  // Dominio alternativo brindado en el email de alta 
-$smtpUsuario = "miCuenta@miDominio.com";  // Mi cuenta de correo
-$smtpClave = "miClave";  // Mi contraseña
+$smtpHost = "c2030870.ferozo.com";  // Dominio alternativo brindado en el email de alta 
+$smtpUsuario = "tenco@tenco.com.ar";  // Mi cuenta de correo
+$smtpClave = "Bonetaso16";  // Mi contraseña
 
 // Email donde se enviaran los datos cargados en el formulario de contacto
-$emailDestino = "correo_destinatario@suDominio.com";
+$emailDestino = "tenco216@gmail.com";
 
 $mail = new PHPMailer();
 $mail->IsSMTP();
@@ -40,15 +40,17 @@ $mail->From = $email; // Email desde donde envío el correo.
 $mail->FromName = $nombre;
 $mail->AddAddress($emailDestino); // Esta es la dirección a donde enviamos los datos del formulario
 
-$mail->Subject = "DonWeb - Ejemplo de formulario de contacto"; // Este es el titulo del email.
+$mail->Subject = "Consulta Web"; // Este es el titulo del email.
 $mensajeHtml = nl2br($mensaje);
 $mail->Body = "{$mensajeHtml} <br /><br />Formulario de ejemplo. By DonWeb<br />"; // Texto del email en formato HTML
 $mail->AltBody = "{$mensaje} \n\n Formulario de ejemplo By DonWeb"; // Texto sin formato HTML
 // FIN - VALORES A MODIFICAR //
 
 $estadoEnvio = $mail->Send(); 
-if($estadoEnvio){
-    echo "El correo fue enviado correctamente.";
+if($estadoEnvio){    
+header("Location:http://tenco.com.ar/#thanks");
+    /*
+    echo "El correo fue enviado correctamente.";*/
 } else {
     echo "Ocurrió un error inesperado.";
 }
